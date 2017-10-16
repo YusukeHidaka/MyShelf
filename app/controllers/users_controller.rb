b5ec8368e434e2d82d0de8c24a7c8ab34c6d7b96
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-16 23:44:56 +0700

    modified user instance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,13 @@ class UsersController < ApplicationController
   end
 
   def shelf
+    @user = User.find(params[:id])
     @shelved_books = ShelvedBook.all.where(user_id: params[:id])
   end
 
   def reviews
-    @reviews = Review.with_user.search_with_user_id(current_user.id)
+    @user = User.find(params[:id])
+    @reviews = Review.with_user.search_with_user_id(params[:id])
   end
 
   private
