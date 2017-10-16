afdf10e28481fce36b1e7c689317f17635d4606f
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-16 17:54:21 +0700

    try many times to search amazon api

--- app/controllers/original_books_controller.rb
+++ app/controllers/original_books_controller.rb
@@ -5,19 +5,31 @@ class OriginalBooksController < ApplicationController
 
   def search
     if params[:keyword].present?
-      #　デバックログ出力するために記述
+      # デバックログ出力するために記述
       Amazon::Ecs.debug = true
 
-      # Amazon::Ecs::Responceオブジェクトの取得
-      books = Amazon::Ecs.item_search(
-        params[:keyword],
-        search_index:  'Books',
-        dataType: 'script',
-        response_group: 'ItemAttributes, Images',
-        sort: 'salesrank',
-        country:  'jp',
-        power: "Not kindle"
-      )
+      # 理不尽に制限に引っかかるので、10回までリトライする設定
+      # https://blog.hello-world.jp.net/ruby/2204/
+      retry_count = 0
+      begin
+        # Amazon::Ecs::Responceオブジェクトの取得
+        books = Amazon::Ecs.item_search(
+          params[:keyword],
+          search_index:  'Books',
+          dataType: 'script',
+          response_group: 'ItemAttributes, Images',
+          sort: 'salesrank',
+          country:  'jp',
+          power: "Not kindle"
+        )
+      rescue => e
+        retry_count += 1
+        logger.error e.message
+        if retry_count < 10
+          sleep(3)
+          retry
+        end
+      end
 
       # 本のタイトル,画像URL, 詳細ページURLの取得
       @amazon_books = []
