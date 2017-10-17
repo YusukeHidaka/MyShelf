cb968db0b22e2e05f1badb64a2815a747ef49964
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-17 23:48:31 +0700

    sort & add path to shelf and reviews & display images in shelf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
 
   def shelf
     @user = User.find(params[:id])
-    @shelved_books = ShelvedBook.all.where(user_id: params[:id])
+    @shelved_books = ShelvedBook.all.where(user_id: params[:id]).order("created_at DESC")
   end
 
   def reviews
     @user = User.find(params[:id])
-    @reviews = Review.with_user.search_with_user_id(params[:id])
+    @reviews = Review.with_user.search_with_user_id(params[:id]).order("created_at DESC")
   end
 
   private
