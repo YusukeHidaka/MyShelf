c77088ff6d41fc5f5e431bd421904443eb58ff23
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-18 00:47:56 +0700

    arrange SPA shelf

--- app/javascript/packs/shelf.js
+++ app/javascript/packs/shelf.js
@@ -1,27 +1,18 @@
 'use strict';
-
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 
-class Shelf extends Component {
+class Shelf extends React.Component {
   render() {
     const {t} = this.props;
 
     return (
       <div>
-        <h1>本棚です。</h1>
-        <h1>データの受け渡しなどはのちにします。</h1>
+        <h1>reactの本棚です。</h1>
       </div>
     );
   }
 }
-
-export default Shelf;
-
-
-// Hello.defaultProps = {
-//   name: 'David'
-// }
-//
-// Hello.propTypes = {
-//   name: PropTypes.string
-// }
+document.addEventListener("DOMContentLoaded", e => {
+  ReactDOM.render(<Shelf />, document.getElementById('ones-shelf'))
+})
