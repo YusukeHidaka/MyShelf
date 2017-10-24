7dcdeb9d583f4ceaaa86bef32cd44de9e82d2bf9
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-25 00:35:07 +0700

    on the way

--- app/models/original_book.rb
+++ app/models/original_book.rb
@@ -4,4 +4,12 @@ class OriginalBook < ApplicationRecord
   # presence: true,
   uniqueness: true
   has_many :shelved_books, dependent: :destroy
+
+  def self.count_shelved(t_book)
+    ShelvedBook.where(original_book_id: t_book).count
+  end
+
+  def self.count_reviews(t_book)
+    Review.with_shelved_book.search_with_original_book_id(t_book).count
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -7,8 +7,9 @@ class Review < ApplicationRecord
    likes.find_by(user_id: user_id)
   end
 
-  # scope :with_shelved_book, -> { joins(:shelved_book) }
+  scope :with_shelved_book, -> { joins({:shelved_book => :original_book}) }
   scope :with_user, -> { joins({:shelved_book => :user}) }
   # scope :with_original_book, -> { joins({:shelved_book => :original_book}) }
+  scope :search_with_original_book_id, ->(original_book_id) { where(:original_books => {:id => original_book_id}) }
   scope :search_with_user_id, ->(user_id) { where(:users => {:id => user_id}) }
 end
