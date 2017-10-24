38dafe024a36d991b6680fd701870fbc6b158ca8
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-25 02:16:58 +0700

    finished review page

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -12,4 +12,24 @@ module ReviewsHelper
     like = Like.find_by(review_id: review["id"], user_id: current_user.id)
     like.id
   end
+
+  def check_reviewed(isbn)
+    original_book = OriginalBook.find_by(isbn: isbn)
+    if original_book.present?
+      shelved_book = ShelvedBook.find_by(original_book_id: original_book.id, user_id: current_user.id)
+      if shelved_book.present?
+        review = Review.find_by(shelved_book_id: shelved_book.id)
+        if review.present?
+          return review
+        else
+          return false
+        end
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
 end
