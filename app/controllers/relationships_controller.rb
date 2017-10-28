74ea3b36ba164100b001fd3976dc69ff23226229
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-28 17:51:59 +0700

    enable ajax for follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,33 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:following_id])
     current_user.follow!(@user)
-    # redirect_to @user
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "relationshipsのcreate"
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
   end
 
   def destroy
     @user = Relationship.find(params[:id]).following
     current_user.unfollow!(@user)
-    # redirect_to @user
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "relationshipsのdestroy"
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
+    puts "=================================="
   end
 
   private
