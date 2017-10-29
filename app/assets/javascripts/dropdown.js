0f18800ff9edfc232a0f64b0d2b136416246765b
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-30 00:40:44 +0700

    make it responsive

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,3 +1,4 @@
+// レビュー用
 $(window).on('turbolinks:load', function(){
   $('.Review__head__setting i').click(function() {
     // メニュー表示/非表示
@@ -21,6 +22,8 @@ $(window).on('turbolinks:load', function(){
   });
 });
 
+
+// ユーザーメニュー用
 $(window).on('turbolinks:load', function(){
   $('.UserIcon__pic').click(function() {
     // メニュー表示/非表示
@@ -40,6 +43,30 @@ $(window).on('turbolinks:load', function(){
   $('body').click(function() {
     if (over_flg == false) {
       $('.UserIcon__menu').slideUp('fast');
+    }
+  });
+});
+
+// スマホメニュー用
+$(window).on('turbolinks:load', function(){
+  $('.Header__trigger').click(function() {
+    // メニュー表示/非表示
+    $(this).next('.Header__menu').slideToggle('fast');
+  });
+
+  var over_flg;
+
+  //　マウスカーソルの位置（メニュー上/メニュー外）
+  $('.Header__trigger,.Header__menu').hover(function(){
+    over_flg = true;
+  }, function(){
+    over_flg = false;
+  });
+
+  // メニュー領域外をクリックしたらメニューを閉じる
+  $('body').click(function() {
+    if (over_flg == false) {
+      $('.Header__menu').slideUp('fast');
     }
   });
 });
